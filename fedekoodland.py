9626ae23c5374b672da6cf4a494ed7feb888e69f
Ociredef11 <truminus@gmail.com>
2024-11-15 18:10:04 +0100

    Create fedekoodland.py

---
+++ fedekoodland.py
@@ -0,0 +1,15 @@
+None
+
+meme_dict = {
+            "CRINGE": "Qualcosa di eccezionalmente strano o imbarazzante",
+            "LOL": "Una risposta comune a qualcosa di divertente",
+            }
+
+parola = input("Scrivi una parola che non capisci (usa solo lettere maiuscole!): ")
+
+if parola in meme_dict.keys():
+    print(meme_dict[parola])
+    # Cosa fare se la parola è stata trovata?
+else:
+    # Cosa fare se la parola non è stata trovata?
+    print("nn conosco quest parola")
